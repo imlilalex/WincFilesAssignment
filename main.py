a42a239885fc30eb02d538852f47f8da5a397a29
imlilalex <112713301+imlilalex@users.noreply.github.com>
2022-10-20 14:44:50 +0200

    Update main.py
    Second Version - updated for more generic use.

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
-# part 1
 
+import os
+import glob
+import zipfile
 from importlib.resources import contents
 
+# part 1
 
 def clean_cache():
-    import os
-    import glob
-
-    cache = r"C:\Users\LilAlex\Documents\wincprojects\files\cache"
+    cwd = os.getcwd()
+    cwd = os.path.join(cwd, "files")
+    cache_name = "cache"
+    cache = os.path.join(cwd, cache_name)
         
     if not os.path.exists(cache):    
         os.mkdir(cache)
         return "New cache created!"
     elif os.path.exists(cache):
-        cache = glob.glob(r"C:\Users\LilAlex\Documents\wincprojects\files\cache\*")
+        cache = os.path.join(cache, "*")
+        cache = glob.glob(cache)
         for files in cache:
             os.remove(files)
         return "The existing cache was cleaned!"
@@ -26,20 +30,19 @@ print(clean_cache())
 #part 2
 
 def cache_zip(file_path, cache_path):
-    import zipfile
-
     zip = zipfile.ZipFile(file_path)
     zip.extractall(cache_path)
     return "Unzipped file to cache"
 
-print(cache_zip(r"C:\Users\LilAlex\Documents\wincprojects\files\data.zip", r"C:\Users\LilAlex\Documents\wincprojects\files\cache"))
+data = os.path.join(os.getcwd(), "files", "data.zip")
+cache_location = os.path.join(os.getcwd(), "files", "cache")
+
+print(cache_zip(data, cache_location))
 
 # part 3
 
 def cached_files():
-    import os
-    
-    cache = r"C:\Users\LilAlex\Documents\wincprojects\files\cache"
+    cache = os.path.join(os.getcwd(), "files", "cache")
     onlyfiles = [os.path.join(cache, f) for f in os.listdir(cache) if 
     os.path.isfile(os.path.join(cache, f))]
     return onlyfiles
